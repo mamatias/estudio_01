adb725c27f8dfe8500dbb9c4409f770f20cf29ea
Matías <matiastorresrisso@gmail.com>
2019-10-04 13:52:11 -0300

    Pequeñas modificaciones y funciones

--- .ipynb_checkpoints/util-checkpoint.py
+++ .ipynb_checkpoints/util-checkpoint.py
@@ -3,3 +3,16 @@ def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='latin1')
     return dict
+
+def translateDicc():
+    return ['avión', 'auto', 'pájaro',
+            'gato', 'ciervo', 'perro',
+            'rana', 'caballo', 'barco',
+            'camión']
+
+from numpy import min, max, zeros
+
+def customNormalize(in_np_arr):
+    min_val = min(in_np_arr)
+    max_val = max(in_np_arr)
+    return (in_np_arr-min_val)/(max_val-min_val)
